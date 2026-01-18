6916343c0c37c6deeafbf9d00b4146043a084acc
Muhammad Altaf <zmaltaf99@gmail.com>
2026-01-19 00:41:52 +0500

    Refactor counter and add number checking functions

--- script.js
+++ script.js
@@ -1,33 +1,76 @@
 var count = 0;
+var max = 33;
 
-document.getElementById("inc").onclick = function(){
-    count++;
-    document.getElementById("num").innerText = count;
+function add(){
+    if(count < max){
+        count++;
+        document.getElementById("display").innerText = count;
+
+        if(count === max){
+            alert("Tasbih Completed");
+        }
+    }
 }
 
-document.getElementById("dec").onclick = function(){
-    count--;
-    document.getElementById("num").innerText = count;
+function minus(){
+    if(count > 0){
+        count--;
+        document.getElementById("display").innerText = count;
+    }
 }
 
-document.getElementById("reset").onclick = function(){
+function clearCount(){
     count = 0;
-    document.getElementById("num").innerText = count;
+    document.getElementById("display").innerText = count;
 }
 
-// second.....
 
-var pass = document.getElementById("pass");
-var icon = document.getElementById("icon");
+// Password toggle
+function togglePass(){
+    var p = document.getElementById("password");
 
-icon.onclick = function(){
-    if(pass.type === "password"){
-        pass.type = "text";
-        icon.textContent = "👁️"; 
+    if(p.type === "password"){
+        p.type = "text";
     }else{
-        pass.type = "password";
-        icon.textContent = "👁️"; 
+        p.type = "password";
     }
 }
 
 
+// Even Odd + Prime
+function checkNum(){
+    var n = document.getElementById("numberInput").value;
+    var eo = document.getElementById("eoResult");
+    var prime = document.getElementById("primeResult");
+
+    if(n === ""){
+        eo.innerText = " Enter a number";
+        eo.className = "red";
+        prime.innerText = "";
+        return;
+    }
+
+    if(n % 2 == 0){
+        eo.innerText = n + " is Even";
+        eo.className = "green";
+    }else{
+        eo.innerText = n + " is Odd";
+        eo.className = "red";
+    }
+
+    var isPrime = true;
+
+    if(n <= 1){
+        isPrime = false;
+    }else{
+        for(var i=2;i<=Math.sqrt(n);i++){
+            if(n % i === 0){
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    prime.innerText = isPrime ? "Prime Number" : "Not Prime";
+    prime.className = isPrime ? "green" : "red";
+}
